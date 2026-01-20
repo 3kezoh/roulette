de0e7192c748591742648b795d8dcda8792ebea4
3kezoh <stanleycrico@duck.com>
2026-01-20 16:46:28 +0100

    build: upgrade packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,13 @@
 import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
 import { viteSingleFile } from "vite-plugin-singlefile";
-// import devtools from 'solid-devtools/vite';
+import tailwindcss from "@tailwindcss/vite"
 
 export default defineConfig({
   plugins: [
-    /* 
-    Uncomment the following line to enable solid-devtools.
-    For more info see https://github.com/thetarnav/solid-devtools/tree/main/packages/extension#readme
-    */
-    // devtools(),
     solidPlugin(),
     viteSingleFile(),
+    tailwindcss()
   ],
   server: {
     port: 3000,
